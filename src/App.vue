7f1fec3144939112a3d0b15ac69a5daa695da2a7
Poznalex <ishutkin30@gmail.com>
2023-12-22 20:04:30 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="main-page">
+        
+
+    </div>
+  
+</template>
+
+<style scoped>
+.main-page {
+    display: flex;
+    width: 1920px;
+    height: 1080px;
+    background-color: #DBF0F1;
+;
+}
+
+ 
+</style>
