bc040aa845e9239f111084bdfd514e4e3ca2bee9
Poznalex <ishutkin30@gmail.com>
2023-12-23 00:30:58 +0100

    add main.css  fix main  background

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,7 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-    <div class="main-page">
-        
-
-    </div>
-  
+    <div></div>
 </template>
 
-<style scoped>
-.main-page {
-    display: flex;
-    width: 1920px;
-    height: 1080px;
-    background-color: #DBF0F1;
-;
-}
-
- 
-</style>
+<style scoped></style>
