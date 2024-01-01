8ca24cc8567f611e9719c52d9e62f921c59315d1
Poznalex <ishutkin30@gmail.com>
2024-01-01 23:33:16 +0100

    add Main.vue

---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+    <div class='btn__column'>TO DO-18</div>
+    <div class='task'>
+    <p>Cekhov - a good writer</p>
+    </div> 
+</template>
+
+<style>
+.task {
+    display: none;
+}
+
+.btn__column {
+    font-family: 'Montserrat';
+    font-weight: 700;
+    font-size: 16px;
+}
+</style>
