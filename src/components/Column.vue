66ccb82fd6a21586f31e7033fc87378aee298c16
Poznalex <ishutkin30@gmail.com>
2024-01-07 00:40:09 +0100

    add styles for header__title min 768px

--- src/components/Column.vue
+++ src/components/Column.vue
@@ -78,6 +78,7 @@
 
 @media screen and (min-width: 768px) {
     .column__header {
+        align-items: start;
         width: 526px;
         height: 118px;
         float: left;
@@ -85,6 +86,23 @@
         margin-bottom: 0;
         overflow: auto;
     }
+
+    .column__header-title {
+        font-size: 20px;
+        margin: 10px 24px;
+    }
+
+    /* .column__tasks {
+        position: absolute;
+        top: 275px;
+        display: flex;
+        width: 526px;
+        padding: 0;
+        background-color: var(--main-color-white);
+        min-height: 252px;
+        margin: 0 10px;
+        border-radius: 10px;
+    } */
 
     .column--open .column__tasks {
         display: none;
