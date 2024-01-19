415e5a879bb4a2790e5984a906e850e2b97145c9
Poznalex <ishutkin30@gmail.com>
2024-01-19 14:39:37 +0100

    13 task trello fixed pading

---
+++ src/components/TaskCard/TaskCard.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+    name: "taskcard",
+    props: {
+        nameProject: {
+            type: String,
+            default: "",
+        },
+    },
+};
+</script>
+
+<template>
+    <div class="card">
+        <img
+            class="card__avatar"
+            alt="user"
+            src="/src/assets/images/card/AVATAR 1.svg"
+        />
+        <div class="card__info__wrapper">
+            <div class="card__header">AMP Project {{ nameProject }}</div>
+            <div class="card__until">Until 17.05.2021</div>
+            <div class="card__hours">10 hours</div>
+            <div class="card__inner__wrapper">
+                <div class="card__number">BM-675</div>
+                <div class="card__project">
+                    <span class="card__project__title">Project</span>X
+                </div>
+            </div>
+        </div>
+        <div class="card__arrow">
+            <img
+                class="arrow__down"
+                alt="arrow"
+                src="/src/assets/images/card/Arrow down.svg"
+            />
+        </div>
+    </div>
+</template>
+
+<style>
+@import './TaskCard.css'
+</style>
