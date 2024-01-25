609a7706e2626f78383c05815d69f37c16514099
Poznalex <ishutkin30@gmail.com>
2024-01-25 10:13:41 +0100

    change option API to Composition API vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <script setup>
 import Header from "/src/components/Header.vue"
 import Main from "/src/components/Main.vue"
+// import Modal from"/src/components/Modal.vue"
+
+// export default {
+//     components: {
+//     Modal,
+//   },
+  
+//   methods: {
+//     openModal() {
+//       this.$refs.modal.openModal();
+//     },
+//   },
+// };
 </script>
 
 <template>
     <Header />
     <Main />
+    <!-- <button @click="openModal">Открыть модальное окно</button> -->
+    <!-- <modal /> -->
 </template>
 
-<style scoped></style>
+<style scoped>
+button {
+    border: 1px solid black;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,58 @@
+<script setup>
+
+            let showModal=(ref)false;
+        };
+    },
+    methods: {
+        openModal() {
+            this.showModal = true;
+        },
+        closeModal() {
+            this.showModal = false;
+        },
+    },
+};
+</script>
+
+
+<template>
+    <div class="modal" v-if="showModal">
+        <div class="modal-content">
+            <span class="close" @click="closeModal">&times;</span>
+            <p>Это модальное окно!</p>
+        </div>
+    </div>
+</template>
+
+
+
+<style scoped>
+/* Стили для модального окна */
+.modal {
+    display: none;
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal-content {
+    background-color: #fefefe;
+    margin: 15% auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 80%;
+}
+
+.close {
+    color: #aaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+    cursor: pointer;
+}
+</style>
