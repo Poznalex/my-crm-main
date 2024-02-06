3a5950002f0396093a46bd18e65c2dfada41b9b6
Poznalex <ishutkin30@gmail.com>
2024-02-06 14:21:15 +0100

    add title input add  and cancel

--- src/components/Column/CreateColumnMob.vue
+++ src/components/Column/CreateColumnMob.vue
@@ -3,18 +3,35 @@
 
 <template>
     <div class="create-column">
-        <span class="create-column__text">Lorem Ipsum</span>
+        <div class="create-column__text">Create Column</div>
+        <form class="create-column__form">
+            <label for="name">Name</label> <br>
+            <input type="text" id="name" name="name">
+        </form>
+        <div>ADD</div>
     </div>
 </template>
 
 <style scoped>
 .create-column {
     display: flex;
+    flex-direction: column;
     width: 300px;
     height: 212px;
 }
+
 .create-column__text {
-    margin: auto;
+    margin-bottom: 18px;
     color: var(--main-color-black);
+}
+
+.create-column__form {
+    margin-bottom: 32px;
+    width: 100%;
+
+}
+
+#name {
+    width: 97%;
 }
 </style>
