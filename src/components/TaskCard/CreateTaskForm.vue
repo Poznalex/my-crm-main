8cc8e864782402c067754dd95d479977b5eced6c
Poznalex <ishutkin30@gmail.com>
2024-03-05 12:58:41 +0100

    17 finished html+css mobview

--- src/components/TaskCard/CreateTaskForm.vue
+++ src/components/TaskCard/CreateTaskForm.vue
@@ -28,6 +28,7 @@
             </form>
 
             <form class="until">
+                <!-- datapicker will be realised in a next task-->
                 <label class="until-title" for="name">Until</label>
                 <input
                     type="text"
@@ -39,6 +40,7 @@
             </form>
 
             <form class="estimation">
+                <!-- datapicker will be realised in a next task-->
                 <label class="estimation-title" for="name">Estimation time</label>
                 <input
                     type="text"
@@ -50,12 +52,13 @@
             </form>
 
             <form class="author">
+                <!-- choose author will be realised in a next task-->
                 <label class="author-title" for="name">Author</label>
                 <input
                     type="text"
                     id="author"
                     name="name"
-                    placeholder="Placeholder"
+                    placeholder="Automatically"
                     maxlength="500"
                 />
             </form>
@@ -92,11 +95,27 @@ label {
 }
 
 #until {
-    src='' 15px 15px no-repeat;
+    background: url('/src/assets/images/card/calendar.svg') 98% center no-repeat;
+    padding-right: 32px;
+}
+
+#estimation {
+    background: url('/src/assets/images/card/clock.svg') 98% center no-repeat;
+    padding-right: 32px;
+}
+
+#author {
+    background: url('/src/assets/images/card/userpic.svg') 2% center no-repeat,
+        url('/src/assets/images/card/arrow-down  choose author.svg') 95.5% center
+            no-repeat;
+    padding: 8px 32px;
+}
+
+#author::placeholder {
+    color: var(--main-color-black);
 }
 
 .btn-create {
-    /* width: 100%; */
     background-color: var(--main-color-pink);
     font-size: 14px;
     color: var(--main-color-white);
@@ -109,5 +128,6 @@ label {
 .btn-cancel {
     color: var(--main-color-light-blue);
     font-size: 14px;
+    line-height: 10px;
 }
 </style>
