6bd666beceda1e4fa2d0c630984aef2ba1d7f104
Poznalex <98047729+Poznalex@users.noreply.github.com>
2024-11-11 15:47:12 +0100

    18 scroll fix (#22)
    * columns without task. scroll not worked
    
    * style of scroll
    
    * finish scroll
    
    * horizontal track transparent

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,22 +5,23 @@ import CreateColumnForm from '/src/components/Column/CreateColumnForm.vue';
 
 <template>
     <div class="main">
-        <Column
-            title="to do"
-            amountTasks="18"
-            @open-modal="(taskInformation) => $emit('open-modal', taskInformation)"
-        />
-        <Column
-            title="in progress"
-            amountTasks="3"
-            @open-modal="(taskInformation) => $emit('open-modal', taskInformation)"
-        />
-        <Column
-            title="done"
-            amountTasks="456"
-            @open-modal="(taskInformation) => $emit('open-modal', taskInformation)"
-        />
-
+        <div class="columns-wrapper">
+            <Column
+                title="to do"
+                amountTasks="18"
+                @open-modal="(taskInformation) => $emit('open-modal', taskInformation)"
+            />
+            <Column
+                title="in progress"
+                amountTasks="3"
+                @open-modal="(taskInformation) => $emit('open-modal', taskInformation)"
+            />
+            <Column
+                title="done"
+                amountTasks="456"
+                @open-modal="(taskInformation) => $emit('open-modal', taskInformation)"
+            />
+        </div>
         <div class="main__create-column" @click="$emit('open-modal', CreateColumnForm)">
             <div class="create-column__text">Create column</div>
             <div class="create-column__icon">
@@ -45,6 +46,12 @@ import CreateColumnForm from '/src/components/Column/CreateColumnForm.vue';
     cursor: pointer;
 }
 
+.columns-wrapper {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+}
+
 .create-column__text {
     width: auto;
     line-height: 18px;
@@ -66,11 +73,39 @@ import CreateColumnForm from '/src/components/Column/CreateColumnForm.vue';
         justify-content: unset;
         align-items: unset;
         padding: 93px 24px 0;
-        overflow-x: scroll;
+        flex-grow: 1;
+        overflow: hidden;
     }
 
     .main__create-column {
         display: none;
+    }
+
+    .columns-wrapper {
+        display: flex;
+        flex-direction: row;
+        overflow: scroll;
+    }
+
+    .columns-wrapper::-webkit-scrollbar {
+        width: 6px;
+        height: 6px;
+    }
+
+    .columns-wrapper::-webkit-scrollbar-thumb {
+        border-radius: 8px;
+        background: var(--main-color-light-blue);
+    }
+
+    .columns-wrapper::-webkit-scrollbar-track {
+        border-radius: 8px;
+        background-color: var(--main-color-white);
+    }
+
+    .columns-wrapper::-webkit-scrollbar-corner,
+    .columns-wrapper::-webkit-scrollbar-track:horizontal {
+        background-color: transparent;
+        margin-right: 20px;
     }
 }
 
