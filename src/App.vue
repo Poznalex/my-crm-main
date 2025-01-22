f732634bde22bb37229cf3af4f10c96b9573e42e
Poznalex <ishutkin30@gmail.com>
2025-01-22 08:39:10 +0100

    realized draggable columns

--- src/App.vue
+++ src/App.vue
@@ -17,38 +17,32 @@ function closeModal() {
     modalComponent.value = null;
 }
 
-   const savedArrayColumns = JSON.parse(localStorage.getItem('arrColumnsInLocalStorage'));
-   console.log(JSON.parse(localStorage.getItem('arrColumnsInLocalStorage')));
+const savedArrayColumns = JSON.parse(localStorage.getItem('arrColumnsInLocalStorage'));
+console.log(JSON.parse(localStorage.getItem('arrColumnsInLocalStorage')));
 
 function AddColumnToColumnArray(name) {
     const newColumn =
     {
-         title: name,
-         tasksCount: "0",
-         order: savedArrayColumns.length-1,
+        title: name,
+        tasksCount: "0",
+        order: savedArrayColumns.length - 1,
     }
-      console.log(newColumn);
+    console.log(newColumn);
 
-    savedArrayColumns.splice(savedArrayColumns.length-1, 0, newColumn); //вставляем новую колонку  в массив колонок
+    savedArrayColumns.splice(savedArrayColumns.length - 1, 0, newColumn); //вставляем новую колонку  в массив колонок
     window.location.reload()
     localStorage.setItem('arrColumnsInLocalStorage', JSON.stringify(savedArrayColumns)); //сохраняем обновленный массив колонок в локал сторэдж
 };
-localStorage.clear();
+// localStorage.clear();
 
 </script>
 
 <template>
     <Header @open-modal="openModal" />
-    <Main
-        @open-modal="openModal" 
-    />
+    <Main @open-modal="openModal" />
     <Teleport to="body">
-        <Modal
-            v-show="isShowModal"
-            :defaultComponent="modalComponent"
-            @close="closeModal"
-            @add-new-column="AddColumnToColumnArray"
-        >
+        <Modal v-show="isShowModal" :defaultComponent="modalComponent" @close="closeModal"
+            @add-new-column="AddColumnToColumnArray">
         </Modal>
     </Teleport>
 </template>
